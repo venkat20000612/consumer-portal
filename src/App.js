410740aa127c96788fbeb47eee79d93535a32a2b
Annam Venkata Rao <venkataraoannam97@gmail.com>
2025-12-09 15:33:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Shop from "./pages/Shop";
 import ProductDetails from "./pages/ProductDetails";
 import IngredientDetails from "./pages/IngredientDetails";
 import Forum from "./pages/Forum";
 
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Shop />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/ingredient/:name" element={<IngredientDetails />} />
-         <Route path="/forum" element={<Forum />} /> {/* ✔️ Important */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Shop />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/ingredient/:name" element={<IngredientDetails />} />
+      <Route path="/forum" element={<Forum />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
